3a264458d4c7c4ba111f238b5b7859db3e1f1a3a
hayato94087 <hayato94087@gmail.com>
2024-03-30 08:26:02 +0900

    feat: moduleDetectionをforceを設定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
 import { type FC } from "react";
+import { add, money } from "@/lib/arithmetics";
 
 const Home: FC = () => {
+  add(1);
+  console.log(`money is ${money}`); // money is 1
+
+  add(2);
+  console.log(`money is ${money}`); // money is 13
+
+  // 外部からモジュールの変数を変更できないので以下はエラーとなる。
+  // money = 10;
+
   return (
     <div className="">
       <div className="text-lg font-bold">Home</div>
